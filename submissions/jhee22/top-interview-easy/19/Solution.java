d86e34f3e60d206501e2361e1a62c8358be7bb9f
jhee22 <jheehyun0719@gmail.com>
2026-07-25 19:10:16 +0900

    19

---
+++ submissions/jhee22/top-interview-easy/19/Solution.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode tmp = new ListNode(0); 
+        tmp.next = head; 
+
+        ListNode left = tmp; 
+        ListNode right = tmp; 
+
+        for (int i = 0; i < n + 1; i++){
+            right = right.next; 
+        } 
+
+        while (right != null) {
+            left = left.next; 
+            right = right.next; 
+        }
+
+        left.next = left.next.next; 
+        return tmp.next;
+
+        
+    }
+}
